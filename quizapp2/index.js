a1ee90b421de88dd424a4a9a03e3c23a82e47baa
tolgaszr <tolgaszr@gmail.com>
2023-04-25 01:21:36 +0300

    get question and answers

--- quizapp2/index.js
+++ quizapp2/index.js
@@ -51,6 +51,11 @@ getNewQuestion = () => {
     const questionIndex= Math.floor(Math.random()* availableQuestions.length);
     currentQuestion = availableQuestions[questionIndex];
     question.innerText = currentQuestion.question;
+
+    choices.forEach( choice =>{
+        const number = choice.dataset['number'];
+        choice.innerText = currentQuestion['choice' + number]
+    })
 }
 
 startGame();
