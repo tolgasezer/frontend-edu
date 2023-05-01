5e1ea69d67a6cdb0d4201a5f541ba51c8f3c8a93
tolgaszr <tolgaszr@gmail.com>
2023-05-01 05:15:49 +0300

    messed up.

--- quizapp2/index.js
+++ quizapp2/index.js
@@ -1,13 +1,11 @@
-const question= document.getElementById('question');
-const choiceContainer = document.getElementsByClassName('choice-container');
-const choices = Array.from(document.getElementsByClassName('choice-text'));
-const nextBtn = document.getElementById('nxt-btn');
-const restartBtn = document.getElementById('restart-btn')
-
-
+const question = document.getElementById("question");
+const choiceContainer = document.getElementsByClassName("choice-container");
+const choices = Array.from(document.getElementsByClassName("choice-text"));
+const nextBtn = document.getElementById("nxt-btn");
+const restartBtn = document.getElementById("restart-btn");
 
 //butona baslangicta disabled att vermeye calistigimda sorular gelmiyor.
-nextBtn.setAttribute('disabled', '');
+nextBtn.setAttribute("disabled", "");
 // duzeldi ama nasil oldugunu anlamadim... ('disabled, '') seklinde yazmak gerekliymis
 let currentQuestion = {};
 let acceptingAnswers = false;
@@ -16,105 +14,115 @@ let questionCounter = 0;
 let availableQuestions = [];
 
 let questions = [
-    {
-      question: "Inside which HTML element do we put the JavaScript??",
-      choice1: "<script>",
-      choice2: "<javascript>",
-      choice3: "<js>",
-      choice4: "<scripting>",
-      answer: 1
-    },
-    {
-      question: "What is the correct syntax for referring to an external script called 'xxx.js'?",
-      choice1: "<script href='xxx.js'>",
-      choice2: "<script name='xxx.js'>",
-      choice3: "<script src='xxx.js'>",
-      choice4: "<script file='xxx.js'>",
-      answer: 3
-    },
-    {
-      question: " How do you write 'Hello World' in an alert box?",
-      choice1: "msgBox('Hello World');",
-      choice2: "alertBox('Hello World');",
-      choice3: "msg('Hello World');",
-      choice4: "alert('Hello World');",
-      answer: 4
-    }
-  ]
+  {
+    question: "Inside which HTML element do we put the JavaScript??",
+    choiceText: ["<script>", "<javascript>", "<js>", "<scripting>", "deneme"],
+    answer: 1,
+  },
+  {
+    question:
+      "What is the correct syntax for referring to an external script called 'xxx.js'?",
+    choiceText: [
+      "<script href='xxx.js'>",
+      "<script name='xxx.js'>",
+      "<script src='xxx.js'>",
+      "<script file='xxx.js'>",
+      "deneme",
+    ],
+    answer: 3,
+  },
+  {
+    question: " How do you write 'Hello World' in an alert box?",
+    choiceText: [
+      "msgBox('Hello World');",
+      "alertBox('Hello World');",
+      "msg('Hello World');",
+      "alert('Hello World');",
+      "deneme",
+    ],
+    answer: 4,
+  },
+];
 
 const CORRECT_BONUS = 10;
 const MAX_QUESTIONS = questions.length;
 
+console.log(questions[0].choiceText);
 
-startGame = () =>{
-    score= 0;
-    questionCounter = 0;
-    availableQuestions = [...questions];
-    getNewQuestion();
-}
+startGame = () => {
+  score = 0;
+  questionCounter = 0;
+  availableQuestions = [...questions];
+  getNewQuestion();
+};
 
 getNewQuestion = () => {
-    if (availableQuestions.length == 0 || questionCounter > MAX_QUESTIONS ){
-      nextBtn.classList.add('hidden');
-      restartBtn.classList.remove('hidden');
-      
-      console.log(`Your Score is: ${score*CORRECT_BONUS}/${MAX_QUESTIONS*CORRECT_BONUS}`);
-    }
-    questionCounter++;
-    const questionIndex= Math.floor(Math.random()* availableQuestions.length);
-    currentQuestion = availableQuestions[questionIndex];
-    question.innerText = currentQuestion.question;
-    
+  if (availableQuestions.length == 0 || questionCounter > MAX_QUESTIONS) {
+    nextBtn.classList.add("hidden");
+    restartBtn.classList.remove("hidden");
 
-    choices.forEach( choice =>{
-        const number = choice.dataset['number'];
-        choice.innerText = currentQuestion['choice' + number]
-        
-    });
-    availableQuestions.splice(questionIndex, 1);
-    acceptingAnswers= true;
-  
-    
-  
-}
+    console.log(
+      `Your Score is: ${score * CORRECT_BONUS}/${MAX_QUESTIONS * CORRECT_BONUS}`
+    );
+  }
+  questionCounter++;
+  const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+  currentQuestion = availableQuestions[questionIndex];
+  question.innerText = currentQuestion.question;
+  console.log(currentQuestion.choiceText);
 
-choices.forEach(choice => {
-  choice.addEventListener('click', (e) => {
+  choices.forEach((choice) => {
+    const number = choice.dataset["number"]; //burada sinirlaniyor.
+    choice.innerText = currentQuestion.choiceText[number - 1]; //kanser oldum ama adim adim ilerliyorum.
+
+    // if (!(number < currentQuestion.choiceText.length)) {
+    //   //create and add choice-container
+    //   choiceContainer.innerHTML += `<div class="choice-container">`;
+    //   choiceContainer.innerHTML += `<button class="choice-text">${currentQuestion.choiceText[number - 1]}</button>`;;
+    //   choiceContainer.innerHTML += `</div>`;
+
+    // }
+    // console.log(number);
+  });
+  availableQuestions.splice(questionIndex, 1);
+  acceptingAnswers = true;
+};
+
+choices.forEach((choice) => {
+  choice.addEventListener("click", (e) => {
     //console.log(e);
     if (!acceptingAnswers) return;
 
-    //acceptingAnswers = false;
+    acceptingAnswers = false;
     const selectedChoice = e.target;
     const selectedNumber = selectedChoice.dataset["number"];
     //console.log(selectedNumber)
-    if (selectedNumber==currentQuestion.answer){
-      score++
+    if (selectedNumber == currentQuestion.answer) {
+      score++;
     }
-     
-    selectedChoice.classList.add('selected');
+    // if selectedChoice change, change to selected class
+
     
-    
-    
-    
+
+    selectedChoice.classList.add("selected");
+
     //secimin ardindan next butonu enable oluyor ve next butonunda selected class i kaldiriliyor.
-    nextBtn.removeAttribute('disabled');
-    nextBtn.addEventListener('click', ()=>{
-      selectedChoice.classList.remove('selected');
-      nextBtn.setAttribute('disabled', '');
-    } )
-    
-  })
-})
+    nextBtn.removeAttribute("disabled");
+    nextBtn.addEventListener("click", () => {
+      selectedChoice.classList.remove("selected");
+      nextBtn.setAttribute("disabled", "");
+    });
+  });
+});
 
-
-restartQuiz = () =>{
-  window.location.assign('./index.html')
+restartQuiz = () => {
+  window.location.assign("./index.html");
 
   startGame();
-}
+};
 
-nextBtn.addEventListener('click', getNewQuestion);
-restartBtn.addEventListener('click', restartQuiz);
+nextBtn.addEventListener("click", getNewQuestion);
+restartBtn.addEventListener("click", restartQuiz);
 startGame();
 
 //console.log(currentQuestion.answer);
--- quizappv3/index.js
+++ quizappv3/index.js
@@ -1,2 +1,24 @@
 const question = document.getElementById('question');
 const quizArea = document.getElementById('game');
+const choiceContainer = document.getElementById('choice-container');
+let questions = [
+    {
+      question: "Inside which HTML element do we put the JavaScript??",
+      choiceText: ["<script>", "<javascript>", "<js>", "<scripting>"],
+      answer: 1
+    },
+    {
+      question: "What is the correct syntax for referring to an external script called 'xxx.js'?",
+      choiceText: ["<script href='xxx.js'>", "<script name='xxx.js'>", "<script src='xxx.js'>", "<script file='xxx.js'>"],
+      answer: 3
+    },
+    {
+      question: " How do you write 'Hello World' in an alert box?",
+      choiceText: "msgBox('Hello World');", "alertBox('Hello World');", "msg('Hello World');", "alert('Hello World');",
+      answer: 4
+    }
+  ]
+
+  console.log(questions[0].choiceText); //current questıons choice options seçmek için
+
+let a
