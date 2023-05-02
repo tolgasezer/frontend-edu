d545df9180e86b90dd421d830428e4583caa2d48
tolgaszr <tolgaszr@gmail.com>
2023-05-02 20:08:04 +0300

    choiceText fixed

--- quizapp2/index.js
+++ quizapp2/index.js
@@ -16,17 +16,17 @@ let availableQuestions = [];
 let questions = [
   {
     question: "Inside which HTML element do we put the JavaScript??",
-    choiceText: ["<script>", "<javascript>", "<js>", "<scripting>","deneme"],
+    choiceText: ["<script>", "<javascript>", "<js>", "<scripting>"],
     answer: 0
   },
   {
     question: "What is the correct syntax for referring to an external script called 'xxx.js'?",
-    choiceText: ["<script href='xxx.js'>", "<script name='xxx.js'>", "<script src='xxx.js'>", "<script file='xxx.js'>","deneme"],
+    choiceText: ["<script href='xxx.js'>", "<script name='xxx.js'>", "<script src='xxx.js'>", "<script file='xxx.js'>"],
     answer: 2
   },
   {
     question: " How do you write 'Hello World' in an alert box?",
-    choiceText: ["msgBox('Hello World');", "alertBox('Hello World');", "msg('Hello World');", "alert('Hello World');", "deneme"],
+    choiceText: ["msgBox('Hello World');", "alertBox('Hello World');", "msg('Hello World');", "alert('Hello World');"],
     answer: 3
   }
 ]
