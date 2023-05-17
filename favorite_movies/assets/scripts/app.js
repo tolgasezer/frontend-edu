37ab5582d430a6a81d839502412eb9561e9593a5
tolgaszr <tolgaszr@gmail.com>
2023-05-18 00:18:46 +0300

    reset user input added

--- favorite_movies/assets/scripts/app.js
+++ favorite_movies/assets/scripts/app.js
@@ -3,8 +3,16 @@ const modalBlock = document.getElementById('add-modal');
 const backDropBlock = document.getElementById('backdrop');
 const cancelBtn = modalBlock.querySelector('.btn--passive');
 const addBtn = cancelBtn.nextElementSibling;
-console.log(addBtn);
+const userInputs = modalBlock.querySelectorAll('input');
 
+
+const movies = [];
+
+resetUserInput = ()=>{
+    userInputs.forEach(input => {
+        input.value = '';
+    })
+}
 
 toggleMovieModal= ()=>{
     modalBlock.classList.toggle('visible');
@@ -20,10 +28,30 @@ backdropHandler = ()=>{
     toggleMovieModal();
 }
 addMovieHandler = ()=>{
+    const titleValue = userInputs[0].value;
+    const imageUrlValue = userInputs[1].value;
+    const ratingValue = userInputs[2].value;
 
+    if (titleValue === '' || imageUrlValue === '' || ratingValue === '' || +ratingValue< 1 || +ratingValue > 5){
+        alert('Please input valid number ');
+        return;
+    }
+    const newMovie = {
+        title: titleValue,
+        imageUrl: imageUrlValue,
+        rating: ratingValue,
+    }
+    movies.push(newMovie);
+    console.log(movies);
+    toggleMovieModal();
+    resetUserInput();
+};
+cancelMoviehandler = () =>{
+    resetUserInput();
+    toggleMovieModal();
 }
 
 addMovieBtn.addEventListener('click', toggleMovieModal);
-cancelBtn.addEventListener('click', toggleMovieModal);
+cancelBtn.addEventListener('click', cancelMoviehandler);
 backDropBlock.addEventListener('click',backdropHandler);
 addBtn.addEventListener('click', addMovieHandler);
