05514689e8c5214f435c1701cf5642ba0e6a5cec
tolgaszr <tolgaszr@gmail.com>
2023-05-29 02:08:36 +0300

    sss

---
+++ demoMovieApp/assets/scripts/objects.js
@@ -0,0 +1,18 @@
+const addMovieBtn = document.getElementById('add-movie-btn');
+const searchBtn = document.getElementById('search-btn');
+
+addMovieBtn.addEventListener('click', addMovieBtnHandler);
+searchBtn.addEventListener('click', searchBtnHandler); 
+
+
+const addMovieBtnHandler = () =>{};
+
+const searchBtnHandler =() =>{};
+
+const movieObj = {
+    title : 'getuserinput',
+    [extraInfo]: 'getUserInput',
+    
+
+
+}
