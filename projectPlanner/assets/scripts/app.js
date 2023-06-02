7294523fcc7acb09e1d4bc272a2e481657652385
tolgaszr <tolgaszr@gmail.com>
2023-06-02 23:42:25 +0300

    try to switch object between classes

--- projectPlanner/assets/scripts/app.js
+++ projectPlanner/assets/scripts/app.js
@@ -1,7 +1,46 @@
 class Tooltip {}
 
-class ProjectItem {}
+class ProjectItem {
+    constructor(id) {
+        this.id = id;
+        this.connectMoreInfoButton();
+        this.connectSwitchButton();
 
-class ProjectList {}
+    }
 
-class App {}
+    connectMoreInfoButton(){
+
+    }
+    connectSwitchButton() {
+        const projectItemElement = document.getElementById(this.id);
+        const switchBtn = document.querySelector('button:last-of-type');
+        switchBtn.addEventListener('click',)
+    }
+}
+
+class ProjectList {
+    projects = [];
+    
+    constructor(type) {
+        const prjItems = document.querySelectorAll(`#${type}-projects li`);
+        for (const prjItem of prjItems){
+            this.projects.push(new ProjectItem(prjItem.id));
+        }
+        console.log(this.projects);
+    }
+    addProject() {}
+
+    switchProject(projectId) {
+        const projectIndex = this.projects.findIndex(p => p.id === projectId)
+        this.projects.splice(projectIndex, 1);
+    }
+}
+
+class App {
+    static init() {
+        const activeProjectList = new ProjectList('active');
+        const finishedProjectList = new ProjectList('finished');
+    }
+}
+
+App.init();
