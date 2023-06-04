c7f29fb1527619e757cdac7a70760966545bd48f
tolgaszr <tolgaszr@gmail.com>
2023-06-04 05:55:29 +0300

    component class and inherit method created

--- projectPlanner/assets/scripts/app.js
+++ projectPlanner/assets/scripts/app.js
@@ -12,29 +12,50 @@ class DOMHelper {
         destinationElement.append(element);
     }
 }
+class Component {
+    constructor(hostElementId, insertBefore=false) {
+        if(hostElementId){
+            this.hostElement = document.getElementById(hostElementId);
+        }else {
+            this.hostElement = document.body;
+        }
+        this.insertBefore = insertBefore;
+    }
+    detach(){
+        if(this.element){
+            this.element.remove();
+        }
+    }
+    
+    attach() {
+        this.hostElement.insertAdjacentElement(
+            this.insertBefore 
+            ? 'afterbegin' 
+            : 'beforeend', 
+            this.element);
+        
+    }
+}
 
-class Tooltip {
+class Tooltip extends Component {
     constructor(closeNotifierFunction){
-        this.closeNotifierFunction = closeNotifierFunction;
+        super();
+        this.closeNotifier = closeNotifierFunction;
+        this.create();
     }
 
     closeTooltip (){
         this.detach();
-        this.closeNotifierFunction();
+        this.closeNotifier();
     }
-    
-    detach(){
-        this.element.remove();
-    }
-    
-    attach() {
+    create (){
         const tooltipElement = document.createElement('div');
         tooltipElement.className = 'card';
         tooltipElement.textContent = 'Dummy';
         tooltipElement.addEventListener('click', this.closeTooltip.bind(this));
         this.element= tooltipElement;
-        document.body.append(tooltipElement);
     }
+    
 }
 
 class ProjectItem {
