aa3d8554d43f919a25f7a3aa46e211fcfd2e73dc
tolgaszr <tolgaszr@gmail.com>
2023-06-04 18:43:16 +0300

    more info re-positioning

--- projectPlanner/assets/scripts/app.js
+++ projectPlanner/assets/scripts/app.js
@@ -53,7 +53,18 @@ class Tooltip extends Component {
         const tooltipElement = document.createElement('div');
         tooltipElement.className = 'card';
         tooltipElement.textContent = this.text;
-        console.log(this.hostElement.getBoundingClientRect());
+        
+        const hostElPosLeft = this.hostElement.offsetLeft;
+        const hostElPosTop = this.hostElement.offsetTop; 
+        const hostElHeight = this.hostElement.clientHeight;
+        const parentElScrolling = this.hostElement.parentElement.scrollTop;
+
+        const x = hostElPosLeft + 20 ;
+        const y = hostElPosTop + hostElHeight - parentElScrolling - 10;
+
+        tooltipElement.style.position = 'absolute';
+        tooltipElement.style.left = x + 'px';
+        tooltipElement.style.top = y + 'px';
 
         tooltipElement.addEventListener('click', this.closeTooltip.bind(this));
         this.element= tooltipElement;
