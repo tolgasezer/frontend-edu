72fddbd83fa4f6cf7cba5a81bec666b8f6c145f0
tolgaszr <tolgaszr@gmail.com>
2023-06-05 18:11:19 +0300

    setInterval-clearInterval using type

--- projectPlanner/assets/scripts/analytics.js
+++ projectPlanner/assets/scripts/analytics.js
@@ -1,1 +1,8 @@
-console.log('Sending Analytics...');
+
+const intervalId = setInterval(() => {
+  console.log('Sending analytics data...')
+}, 2000);
+
+document.getElementById('stop-analytics-btn').addEventListener('click', () => {
+  clearInterval(intervalId);
+});
