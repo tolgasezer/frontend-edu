a85e20d89bb2cefa9af7f178a38da36e8479446c
tolgaszr <tolgaszr@gmail.com>
2023-06-08 14:53:51 +0300

    get and used data

---
+++ http-starting-setup/assets/scripts/app.js
@@ -0,0 +1,25 @@
+const postElement = document.querySelector('.posts');
+const postTemplate = document.getElementById('single-post');
+
+const xhr = new XMLHttpRequest();
+
+xhr.open('GET','https://jsonplaceholder.typicode.com/posts');
+
+xhr.responseType = 'json'
+
+xhr.onload = function () {
+    const listOfPosts = xhr.response;
+    
+    listOfPosts.forEach(post => {
+        const postEL = document.importNode(postTemplate.content, true);
+        postEL.querySelector('h2').textContent = post.title.toUpperCase();
+        postEL.querySelector('p').textContent = post.body;
+        postElement.append(postEL);
+        
+    });
+
+
+    console.log(listOfPosts);
+}
+
+xhr.send();
