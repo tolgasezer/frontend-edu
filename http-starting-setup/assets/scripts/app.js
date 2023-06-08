270fb981061916cffee83949f2190fa27f88b224
tolgaszr <tolgaszr@gmail.com>
2023-06-08 15:01:59 +0300

    send request with fetch button

--- http-starting-setup/assets/scripts/app.js
+++ http-starting-setup/assets/scripts/app.js
@@ -1,25 +1,30 @@
-const postElement = document.querySelector('.posts');
-const postTemplate = document.getElementById('single-post');
+const postElement = document.querySelector(".posts");
+const postTemplate = document.getElementById("single-post");
+const fetchPostsButton = document
+  .getElementById("available-posts")
+  .querySelector("button");
 
-const xhr = new XMLHttpRequest();
-
-xhr.open('GET','https://jsonplaceholder.typicode.com/posts');
-
-xhr.responseType = 'json'
-
-xhr.onload = function () {
-    const listOfPosts = xhr.response;
-    
-    listOfPosts.forEach(post => {
-        const postEL = document.importNode(postTemplate.content, true);
-        postEL.querySelector('h2').textContent = post.title.toUpperCase();
-        postEL.querySelector('p').textContent = post.body;
-        postElement.append(postEL);
+  
+  const getPosts = () => {
+      const xhr = new XMLHttpRequest();
+      xhr.open("GET", "https://jsonplaceholder.typicode.com/posts");
+      
+      xhr.responseType = "json";
+      
+      xhr.onload = function () {
+          const listOfPosts = xhr.response;
+          
+          listOfPosts.forEach((post) => {
+              const postEL = document.importNode(postTemplate.content, true);
+              postEL.querySelector("h2").textContent = post.title.toUpperCase();
+              postEL.querySelector("p").textContent = post.body;
+              postElement.append(postEL);
+            });
+            
+            console.log(listOfPosts);
+        };
         
-    });
-
-
-    console.log(listOfPosts);
-}
-
-xhr.send();
+        xhr.send();
+    };
+    
+fetchPostsButton.addEventListener("click", getPosts);
