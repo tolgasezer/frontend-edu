50d61b1eb6e23264b1d05507e6b7c89f333ccecb
tolgaszr <tolgaszr@gmail.com>
2023-06-09 17:05:00 +0300

    added method and body args

--- http-starting-setup/assets/scripts/app.js
+++ http-starting-setup/assets/scripts/app.js
@@ -23,7 +23,10 @@ function sendHttpRequest(method, url, data) {
   //   xhr.send(JSON.stringify(data));
   // });
   // return promise;
-  return fetch(url).then(response => {
+  return fetch(url,{
+    method: method,
+    body: JSON.stringify(data)
+  }).then(response => {
     return response.json();
   })
 
