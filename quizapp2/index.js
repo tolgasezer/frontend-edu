6874a2cce0ea4907a68ef35a5b385a0f711ccbab
tolgaszr <tolgaszr@gmail.com>
2023-06-11 01:12:27 +0300

    availableQuestions broken

--- quizapp2/index.js
+++ quizapp2/index.js
@@ -4,53 +4,57 @@ const choices = Array.from(document.getElementsByClassName("choice-text"));
 const nextBtn = document.getElementById("nxt-btn");
 const restartBtn = document.getElementById("restart-btn");
 
-
 let currentQuestion = {};
-//let acceptingAnswers = false;
 let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
 
-let questions = [
-  {
-    question: "Inside which HTML element do we put the JavaScript??",
-    choiceText: ["<script>", "<javascript>", "<js>", "<scripting>"],
-    answer: 0
-  },
-  {
-    question: "What is the correct syntax for referring to an external script called 'xxx.js'?",
-    choiceText: ["<script href='xxx.js'>", "<script name='xxx.js'>", "<script src='xxx.js'>", "<script file='xxx.js'>"],
-    answer: 2
-  },
-  {
-    question: " How do you write 'Hello World' in an alert box?",
-    choiceText: ["msgBox('Hello World');", "alertBox('Hello World');", "msg('Hello World');", "alert('Hello World');"],
-    answer: 3
-  }
-]
+let questions = [];
+fetch(
+  "https://opentdb.com/api.php?amount=10&category=18&difficulty=easy&type=multiple"
+)
+  .then((response) => {
+    return response.json();
+  })
+  .then((loadedQuestions) => {
+    questions = loadedQuestions.results.map((question) => {
+      const formattedQuestion = {
+        question: question.question,
+        choiceText: [...question.incorrect_answers, question.correct_answer],
+        answer: 3,
+      };
 
+      return formattedQuestion;
+    });
+    
+
+    startGame();
+  });
 
 const CORRECT_BONUS = 10;
 const MAX_QUESTIONS = questions.length;
 
-//console.log(questions[0].choiceText);
+
 
 startGame = () => {
   score = 0;
   questionCounter = 0;
   availableQuestions = [...questions];
-  getNewQuestion();
-};
 
+  console.log(availableQuestions)
+  getNewQuestion();
+}
 
 getNewQuestion = () => {
   choiceContainer.innerHTML = "";
   nextBtn.setAttribute("disabled", "");
-  
+
   if (availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
     nextBtn.classList.add("hidden");
     restartBtn.classList.remove("hidden");
-    question.innerText = `Your Score is: ${score * CORRECT_BONUS}/${MAX_QUESTIONS * CORRECT_BONUS}`;
+    question.innerText = `Your Score is: ${score * CORRECT_BONUS}/${
+      MAX_QUESTIONS * CORRECT_BONUS
+    }`;
 
     console.log(
       `Your Score is: ${score * CORRECT_BONUS}/${MAX_QUESTIONS * CORRECT_BONUS}`
@@ -63,32 +67,31 @@ getNewQuestion = () => {
 
   //console.log(currentQuestion.choiceText);
 
-
   //var str = "<ul class='choice-container'>";
-  for(let i = 0; i < currentQuestion.choiceText.length; i++){
-    const newChoice = document.createElement('div');
-    const choiceTextNode = document.createTextNode(`${currentQuestion.choiceText[i]}`);
+  for (let i = 0; i < currentQuestion.choiceText.length; i++) {
+    const newChoice = document.createElement("div");
+    const choiceTextNode = document.createTextNode(
+      `${currentQuestion.choiceText[i]}`
+    );
     newChoice.appendChild(choiceTextNode);
     choiceContainer.appendChild(newChoice);
-    newChoice.classList.add('choice-container'); //prefix i feda ettik ama olsun.
-    
-    newChoice.addEventListener('click', () => {
+    newChoice.classList.add("choice-container"); //prefix i feda ettik ama olsun.
+
+    newChoice.addEventListener("click", () => {
       // Tüm choice-container'ları seçim yapılmamış duruma getir
-      const allChoices = document.querySelectorAll('.choice-container');
-      allChoices.forEach(choice => {
-        choice.classList.remove('selected');
+      const allChoices = document.querySelectorAll(".choice-container");
+      allChoices.forEach((choice) => {
+        choice.classList.remove("selected");
       });
-      
+
       // Seçili olan choice-containera selected classı ekle
-      newChoice.classList.add('selected');
-      
-      
+      newChoice.classList.add("selected");
+
       //console.log(choicesArray.indexOf('div.choice-container.selected'));
       nextBtn.removeAttribute("disabled");
-    });    
+    });
   }
   availableQuestions.splice(questionIndex, 1);
-  
 };
 
 restartQuiz = () => {
@@ -97,55 +100,28 @@ restartQuiz = () => {
   startGame();
 };
 
-nextBtn.addEventListener("click", ()=>{
-  const selected = document.querySelector('.selected');
+nextBtn.addEventListener("click", () => {
+  const selected = document.querySelector(".selected");
   console.log(selected.innerHTML);
-  if (selected.innerText == currentQuestion.choiceText[currentQuestion.answer]){
+  if (
+    selected.innerText == currentQuestion.choiceText[currentQuestion.answer]
+  ) {
     score++;
-  };
-  getNewQuestion()});
+  }
+  getNewQuestion();
+});
 restartBtn.addEventListener("click", restartQuiz);
-
-
 
 //console.log(currentQuestion.choiceText);
 
-function getTriviaQuestions (method, url, data){
-  return fetch(url, {
-    method: method,
-    body: JSON.stringify(data)
-  }).then(response =>{
-    startGame();
-    return response.json();
-  })
-};
+// function getTriviaQuestions (method, url, data){
+//   return fetch(url, {
+//     method: method,
+//     body: JSON.stringify(data)
+//   }).then(response =>{
+//     startGame();
+//     return response.json();
+//   })
+// };
 
-async function fetchTriviaQuestion (){
-  const responseData = await getTriviaQuestions(
-    'GET',
-    'https://opentdb.com/api.php?amount=10&category=18&difficulty=easy&type=multiple'
-    );
-    
-  const listOfPosts = responseData;
-  
-  const newQuestions =listOfPosts.results.map(question => {
-    const formattedQuestion = {
-      question: question.question,
-      choiceText: [...question.incorrect_answers, question.correct_answer],
-      answer: 3
-    }
-    return formattedQuestion;
-  })
-  newQuestions.forEach(question =>{
-    availableQuestions.push(question);
-  })
-  //console.log(listOfPosts.results[0].question);
-  // for(let i = 0; i < listOfPosts.results.length; i++){
-  //  questions.push(listOfPosts.results[i]);
-  // }
-  //console.log(questions);
-  console.log(availableQuestions.length);
-}
-
-fetchTriviaQuestion();
-
+// fetchTriviaQuestion();
